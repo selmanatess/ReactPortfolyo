1556c72418b28431faf7d2e6289f2b6cb89caa39
Selman ATEŞ <selmanates007@gmail.com>
2024-07-06 18:30:44 +0300

    commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -13,27 +13,25 @@ export default function HomePage() {
       <div className='home-text'>
         <div className='header'>
           <div className='hello'>
-            Merhaba, Ben
+            Merhaba, Web Siteme Hoşgeldiniz...
           </div>
-          </div>
-          <div className='container-name'>
-            <div className='home-text-name'>Selman ATEŞ
+        </div>
+        <div className='container-name'>
+          <div className='home-text-name'>Selman ATEŞ</div>
+        </div>
 
-            </div>
-          </div>
-
-          <div className='container'>
-            <span className='text first-text'> Ben bir </span>
-            <Typewriter></Typewriter>
-          </div>
-          <div className='home-text-content'>
-            <p>Front-End Developer olarak çalışmaktayım. <br></br>React, TypeScript, JavaScript, HTML, CSS, SCSS, Bootstrap, Material-UI, Firebase, Git ve Github gibi teknolojileri kullanmaktayım.</p>
-          </div>
-         <div className='button-div'> <button className='resume-button'> Özgeçmiş İndir
-         </button></div>
-        
-
-
+        <div className='container'>
+          <span className='text first-text'> Ben bir </span>
+          <Typewriter></Typewriter>
+        </div>
+        <div className='home-text-content'>
+          <p>Front-End Developer olarak çalışmaktayım. <br></br>React, TypeScript, JavaScript, HTML, CSS, SCSS, Bootstrap, Material-UI, Firebase, Git ve Github gibi teknolojileri kullanmaktayım.</p>
+        </div>
+        <div className='button-div'> 
+          <button className='resume-button'> Özgeçmiş İndir
+        </button>
+        </div>
+       
       </div>
       <div className='my-photos'>
         <img src={myphoto} alt="myphoto" />
